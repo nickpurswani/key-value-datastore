64a42e2e88e0a62d960c3c40b1ce9895aa807f69
nickpurswani2898 <nikhilopp66@gmail.com>
2020-12-27 20:41:34 +0530

    init

---
+++ main.py
@@ -0,0 +1,9 @@
+#this is to make key-value data store using python3
+#for freshworks problem statement
+#code begins 
+#--------------------------------------------------
+import threading 
+from threading import*
+import time
+
+data={} #dictionary to store data
