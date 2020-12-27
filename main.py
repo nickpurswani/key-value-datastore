0474798874bd259f6152bdd44ccdbcd84dceac00
nickpurswani2898 <nikhilopp66@gmail.com>
2020-12-27 22:42:59 +0530

    read

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ import json
 data={} #dictionary to store data
 #for create operation 
 #use syntax "create(key_name,value,timeout_value)" timeout is in seconds
-
-def create(key,value,timeout=-1):#initial timeout -1 means infinite time 
+def create(key,value,timeout=0):#initial timeout 0 means infinite time 
     if key in data:
         print("error: this key already exists") #error
     else:
@@ -23,9 +22,34 @@ def create(key,value,timeout=-1):#initial timeout -1 means infinite time
                     l=[value,time.time()+timeout]
                 if len(key)<=32: # key_name capped at 32chars
                     data[key] = l
-                with open('data.txt', 'w') as outfile:
+                with open('data.json', 'w') as outfile:
                     json.dump(data, outfile)
             else:
                 print("error: Memory limit exceeded!! ")#error
         else:
             print("error: Invalind key_name!! key_name must contain only alphabets and no special characters or numbers")#error 
+#for read operation
+#use syntax "read(key_name)"
+            
+def read(key):
+    try:
+        fo = open("data.json", "r")
+        data = json.load(fo)
+        print(data)
+        pass
+        fo.close()
+    except IOError:
+        data = {}
+    if key not in data:
+        print("error: given key does not exist in database. Please enter a valid key") #error message4
+    else:
+        b=data[key]
+        if b[1]!=0:
+            if time.time()<b[1]: #comparing the present time with expiry time
+                stri=str(key)+":"+str(b[0]) #to return the value in the format of JasonObject i.e.,"key_name:value"
+                return stri
+            else:
+                print("error: time-to-live of",key,"has expired") #error message5
+        else:
+            stri=str(key)+":"+str(b[0])
+            return stri
